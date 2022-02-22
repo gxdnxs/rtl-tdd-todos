b11a073203cda717387a7b161be79c873cd6ae88
gydnjs <projectann215@gmail.com>
2022-02-22 15:42:39 +0900

    todolist test ok

--- src/TodoList.test.js
+++ src/TodoList.test.js
@@ -1,23 +1,38 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import TodoList from "./TodoList";
 
-describe('<TodoList />' , () => {
+describe("<TodoList />", () => {
     const sampleTodos = [
         {
             id: 1,
-            text: 'Learn TDD',
-            done: true
+            text: "Learn TDD",
+            done: true,
         },
         {
             id: 2,
-            text: 'use react-testing-library',
-            done: true
-        }
+            text: "use react-testing-library",
+            done: true,
+        },
     ];
-    it('renders todos properly', () => {
-        const {getByText} = render(<TodoList todos={sampleTodos} />)
+    it("renders todos properly", () => {
+        const { getByText } = render(<TodoList todos={sampleTodos} />);
         getByText(sampleTodos[0].text);
         getByText(sampleTodos[1].text);
-    })
-})
+    });
+    it("calls onToggle and onRemove", () => {
+        const onToggle = jest.fn();
+        const onRemove = jest.fn();
+        const { getByText, getAllByText } = render(
+            <TodoList
+                todos={sampleTodos}
+                onToggle={onToggle}
+                onRemove={onRemove}
+            />
+        );
+        fireEvent.click(getByText(sampleTodos[0].text));
+        expect(onToggle).toBeCalledWith(sampleTodos[0].id);
+        fireEvent.click(getAllByText('삭제')[0]);
+        expect(onRemove).toBeCalledWith(sampleTodos[0].id);
+    });
+});
